c371f735899f9a31d2925ace47f6eebb5627263d
Esnglr <esinguler.eglr@proton.me>
2026-05-24 01:36:44 +0300

    vfs logic is implemented

---
+++ atom/src/vfs/vfs.rs
@@ -0,0 +1,69 @@
+use std::os::fd::{OwnedFd, AsRawFd, FromRawFd};
+use std::ffi::CString;
+use std::io::{Write, Read, Seek, SeekFrom, Result as IoResult, Error, ErrorKind};
+use nix::sys::memfd::{memfd_create, MFdFlags};
+use nix::unistd::{ftruncate, lseek, Whence};
+pub struct MemFile {
+    fd: OwnedFd,
+    fixed_size: usize,
+}
+
+impl MemFile {
+    pub fn new(vault_name: &str, vault_size: usize) -> Result<Self, Box<dyn std::error::Error>>{
+        let vault_name = CString::new(vault_name)?;
+
+        // we want from linux to create an empty file for us
+        let fd = memfd_create(&vault_name, MFdFlags::MFD_CLOEXEC)?;
+
+        // we want vault to be fixed size
+        ftruncate(fd, vault_size as i64)?;
+
+        //let smart_fd = unsafe { OwnedFd::from_raw_fd(fd) };
+
+        Ok(Self { 
+            fd, 
+            fixed_size: vault_size
+        })        
+    }
+}
+
+// we are making a contract with the trait of std::io::write so that our file is not just any file but can write 
+impl Write for MemFile {
+    fn write(&mut self, buf: &[u8]) -> IoResult<usize>{
+        let written_size = nix::unistd::write(self.fd.as_raw_fd(), buf)
+            .map_err(|e| Error::new(ErrorKind::Other, e))?;
+        Ok(written_size)     
+    }
+
+    fn flush(&mut self) -> IoResult<()>{
+        Ok(())
+    }
+}
+
+impl Read for MemFile {
+    fn read(&mut self, buf: &mut [u8]) -> IoResult<usize>{
+        let read_size = nix::unistd::read(self.fd.as_raw_fd(), buf)
+            .map_err(|e| Error::new(ErrorKind::Other, e))?;
+        Ok(read_size)
+    }
+}
+
+impl Seek for MemFile {
+    fn seek(&mut self, pos: SeekFrom) -> IoResult<u64> {
+        let (offset, whence) = match pos {
+            SeekFrom::Start(n) => (n as i64, Whence::SeekSet),
+            SeekFrom::Current(n) => (n, Whence::SeekCur),
+            SeekFrom::End(n) => (n, Whence::SeekEnd),
+        };
+
+        let new_pos = lseek(self.fd.as_raw_fd(), offset, whence)
+            .map_err(|e| Error::new(ErrorKind::Other, e))?;
+
+        Ok(new_pos as u64)
+    }
+}
+
+
+fn main() {
+
+}
